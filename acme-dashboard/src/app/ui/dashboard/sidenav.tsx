72f84b737c0bfa5798972c7e058c29950f4a43f2
Gohouri <angejunior246@gmail.com>
2025-09-12 00:01:12 +0000

    correction1

---
+++ acme-dashboard/src/app/ui/dashboard/sidenav.tsx
@@ -0,0 +1,38 @@
+import { PowerIcon } from '@heroicons/react/24/outline';
+import NavLinks from './nav-links';
+
+export default function SideNav() {
+  return (
+    <div className="flex h-full flex-col px-3 py-4 md:px-2">
+      <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
+        <div className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-blue-600 text-white p-3 text-sm font-medium md:flex-none md:justify-start md:p-2 md:px-3">
+          <div className="w-5">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-5 h-5"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3s-4.5 4.03-4.5 9 2.015 9 4.5 9z"
+              />
+            </svg>
+          </div>
+          <div className="hidden md:block font-semibold">Acme</div>
+        </div>
+        <NavLinks />
+        <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
+        <form>
+          <button className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+            <PowerIcon className="w-6" />
+            <div className="hidden md:block">Sign Out</div>
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
